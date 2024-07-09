5fcdd07df5fd4a042ef6b08f995f46f918a01033
Sam Sweeney <samsweeneyy@gmail.com>
2024-07-09 21:38:52 +1000

    docker

--- Dockerfile
+++ Dockerfile
@@ -1,29 +1,42 @@
-FROM node:18-alpine AS builder
+# Stage 1: Building the app
+FROM node:20-alpine AS builder
 
+# Set the working directory in the Docker container
 WORKDIR /app
 
+# Copy the package.json and package-lock.json (or yarn.lock) files
 COPY package*.json ./
 
-CMD ["npm", "install"]
+# Install dependencies
+RUN npm ci
 
-# Copy the local code to the container image.
+# Copy the rest of your app's source code from your host to your image filesystem.
 COPY . .
 
+
+# Build the Next.js application
 RUN npm run build
 
-# Use a separate image for the runtime environment.
-FROM node:18-alpine AS runner
-
+# Stage 2: Run the app in production mode
+FROM node:20-alpine AS runner
 WORKDIR /app
 
+
+
+# Copy the build output from the builder stage
+COPY --from=builder /app/next.config.mjs ./
+COPY --from=builder /app/public ./public
 COPY --from=builder /app/.next ./.next
 COPY --from=builder /app/node_modules ./node_modules
-COPY --from=builder /app/package*.json ./
-COPY --from=builder /app/public ./public
+COPY --from=builder /app/package.json ./package.json
 
-RUN npm install -g typescript
+ARG NODE_ENV=production
+ENV NODE_ENV=$NODE_ENV
+ARG MONGO_URI=mongodb://db:27017/test
+ENV MONGO_URI=$MONGODB_URI
 
-COPY ./scripts/seed.ts ./
-RUN node --loader ts-node/esm seed.ts
+# Expose the port Next.js runs on
+EXPOSE 3000
 
-CMD ["npm", "run", "start"]
+# Command to run the app
+CMD ["npm", "start"]
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,43 @@
 import connectMongo from "@/lib/connect-mongo";
 import { Blog, IBlog } from "@/models/post";
-
-
+import { revalidatePath } from "next/cache";
 async function getBlogs() {
   await connectMongo();
   const blogs = await Blog.find();
-  return blogs;
+  return JSON.parse(JSON.stringify(blogs)) as IBlog[];
+}
+
+async function createBlog(data: FormData) {
+  "use server";
+  await connectMongo();
+  const blog = new Blog({
+    name: "Blog 4",
+    description: "Description 4",
+  });
+  revalidatePath("/");
+  await blog.save();
 }
 
 export default async function Home() {
-  const blogs = await getBlogs() as IBlog[];
+  const blogs = (await getBlogs()) as IBlog[];
   return (
     <div className="container mx-auto">
+      {JSON.stringify(blogs)}
       <div className="grid grid-cols-4 text-white">
-        {JSON.stringify(blogs)}
-        {
-          blogs.map((blog) => (
-            <div key={blog._id} className="p-4">
-              <h1 className="text-2xl font-bold">{blog.name}</h1>
-              <p>{blog.description}</p>
-            </div>
-          ))
-        }
+        <form className="p-4" action={createBlog}>
+          <input type="text" name="name" placeholder="Name" />
+          <input type="text" name="description" placeholder="Description" />
+          <button type="submit">Create</button>
+        </form>
+        {blogs.map((blog) => (
+          <div key={blog._id} className="p-4">
+            <h1 className="text-2xl font-bold">{blog.name}</h1>
+            <p>{blog.description}</p>
+          </div>
+        ))}
       </div>
     </div>
-
   );
 }
+
+export const dynamic = "force-dynamic";
